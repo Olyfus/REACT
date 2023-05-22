e826417445b0df837fe9e0dc393312d0a319e267
Olyfus <60556656+Olyfus@users.noreply.github.com>
2023-05-22 15:45:18 +0200

    Card en place

--- rogue-palace/src/Cards/PersonnageCard.js
+++ rogue-palace/src/Cards/PersonnageCard.js
@@ -5,23 +5,38 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 function PersonnageCard(props) {
-    
-    const { perso } = props;
-    
+    const [groupe, setGroupe] = useState([]);
+
+    axios.get(`https://localhost:7183/groupe/${props.personnage.groupeId}`, {
+        headers: { Authorization: 'Bearer '+localStorage.getItem("access_token")+''}
+    }).then((res) => {
+        console.log(res.data);
+        setGroupe(res.data);
+    }).catch((err) => {
+        console.log(err);
+    })
+
     return (
-        <div className="App">
-            <div>
-                <Card sx={{ maxWidth: '80%', m: 2, p:2, position: 'sticky', left:'8.5%'}}>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                                Nom : {props.personnage.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                                {props.personnage.id}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </div>
+        <div className="Card_Personnage">
+            <Card sx={{ maxWidth: '20%', m: 2, p:2, position: 'sticky', left:'8.5%'}}>
+                <CardContent>
+                    <Typography gutterBottom variant="h4" component="div">
+                        {props.personnage.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Classe {props.personnage.class}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Race {props.personnage.race}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Niveau {props.personnage.level}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Groupe : {groupe.name}
+                    </Typography>
+                </CardContent>
+            </Card>
         </div>
     );
 };
